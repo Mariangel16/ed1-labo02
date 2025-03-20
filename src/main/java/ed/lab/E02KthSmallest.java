62853a8bc14cc2a6da55be2cb828522fb89fb893
Mariangel16 <84152916+Mariangel16@users.noreply.github.com>
2025-03-19 22:21:14 -0600

    Cambios ejercicio 2

--- src/main/java/ed/lab/E02KthSmallest.java
+++ src/main/java/ed/lab/E02KthSmallest.java
@@ -1,9 +1,30 @@
 package ed.lab;
+import java.util.Stack;
 
 public class E02KthSmallest {
 
     public int kthSmallest(TreeNode<Integer> root, int k) {
-        return 0;
+
+        Stack<TreeNode<Integer>> stack = new Stack<>();
+        TreeNode<Integer> current = root;
+        int count = 0;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            count++;
+
+            if (count == k) {
+                return current.value;  // Usamos 'value' en lugar de 'val'
+            }
+
+            current = current.right;
+        }
+        return -1; // Si k es inválido
     }
 
 }
